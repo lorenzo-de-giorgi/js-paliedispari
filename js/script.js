9b81750a2335b36b1945740c92e2bc57e756a9fe
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-14 13:42:48 +0100

    ask input for pari o dispari exercise

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
     Creare una funzione per capire se la parola inserita è palindroma
 */
 
+// input parola
 let parola = prompt('Inserisci una parola');
 
 function palindroma() {
@@ -25,3 +26,16 @@ function palindroma() {
 }
 
 console.log(palindroma(parola))
+
+/*
+    L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    Sommiamo i due numeri
+    Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    Dichiariamo chi ha vinto.
+*/
+
+// input pari o dispari
+let pOd = prompt('Scegli pari o dispari');
+// input numero
+let num = parseInt(prompt('Scegli un numero tra uno e 5'));
